30b8ebb69de2206fc4c30798aecacaab764aa663
Sebastian Gubiec <seb.gub@wp.pl>
2021-06-08 14:02:28 +0200

    5.3fix

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,9 @@
-let computerMove = 'kamień';
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove= 'papier';
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+let computerMove = 'kamień';
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove= 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
