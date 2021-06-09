4f2494098a79c50050feade2aad72521c03d24d0
Sebastian Gubiec <seb.gub@wp.pl>
2021-06-09 21:25:21 +0200

    fix5.4

--- js/script.js
+++ js/script.js
@@ -34,22 +34,14 @@ if( computerMove == 'kamień' && playerMove == 'papier'){
   printMessage('Ty wygrywasz!');
 }else if( computerMove == 'kamień' && playerMove == 'nożyce'){
   printMessage('Ty przegrywasz!');
-}else if( computerMove == 'kamień' && playerMove == 'kamień'){
-  printMessage('Mamy remis!');
-}
-
-if( computerMove == 'papier' && playerMove == 'nożyce'){
+}else if( computerMove == 'papier' && playerMove == 'nożyce'){
   printMessage('Ty wygrywasz!');
 }else if( computerMove == 'papier' && playerMove == 'kamień'){
   printMessage('Ty przegrywasz!');
-}else if( computerMove == 'papier' && playerMove == 'papier'){
-  printMessage('Mamy remis!');
-}
-
-if( computerMove == 'nożyce' && playerMove == 'kamień'){
+}else if( computerMove == 'nożyce' && playerMove == 'kamień'){
   printMessage('Ty wygrywasz!');
 }else if( computerMove == 'nożyce' && playerMove == 'papier'){
   printMessage('Ty przegrywasz!');
-}else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-  printMessage('Mamy remis!');
+}else if( computerMove == playerMove) {
+  printMessage('Mamy remis!')
 }
